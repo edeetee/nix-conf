db6cfe6712f91f33debecb94c328a767a5f3e246
Edward Taylor <dev@edt.nz>
2024-03-06 09:06:53 +1300

    store hardware conf

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flamenco.path = "/home/edeetee/dev/flamenco";
+  inputs.flamenco.url = "path:/home/edeetee/dev/flamenco-nix";
 
   outputs = { self, nixpkgs, ... }@attrs: {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
-      modules = [ ./configuration.nix ];
+      modules = [./configuration.nix ./ati-server-hardware-configuration.nix ];
     };
   };
 }
