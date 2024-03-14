d4abc3efbcc93c5bd82edf1e1de12f1e40044693
Edward <edeetee@gmail.com>
2024-03-14 20:07:32 +1300

    super tabs

--- neovim/default.nix
+++ neovim/default.nix
@@ -1,11 +1,11 @@
 
-{ ... }:
+{ pkgs, ... }:
 {
     # https://github.com/nix-community/nixvim/tree/main
     programs.nixvim = {
             enable = true;
         #     defaultEditor = true;
-
+			
             colorschemes.gruvbox.enable = true;
             options = {
                     number = true;
@@ -16,10 +16,11 @@
 					scrolloff = 6;
             };
 
+			
+
             keymaps = [
 				{
                     key = " ";
-                    mode = "n";
                     action = "<Nop>";
             	}
 				{
@@ -31,23 +32,60 @@
 					action = "<cmd>Neogit<CR>";
 				}
 				{
-					key = "<leader>t";
+					key = "<leader>tf";
 					action = "<cmd>FloatermToggle<CR>";
 				}
+				{
+					key = "<leader>t";
+					action = "<cmd>terminal<CR>";
+				}
+				{
+					key = "<Esc>";
+					action = ''<C-\><C-n>'';
+					mode = "t";
+				}
 			];
+
+
+			extraConfigVim = ''
+				" 🐓 Coq completion settings
+
+				" Set recommended to false
+				let g:coq_settings = { "keymap.recommended": v:false }
+
+			" Keybindings
+				ino <silent><expr> <Esc>   pumvisible() ? "\<C-e><Esc>" : "\<Esc>"
+				ino <silent><expr> <C-c>   pumvisible() ? "\<C-e><C-c>" : "\<C-c>"
+				ino <silent><expr> <BS>    pumvisible() ? "\<C-e><BS>"  : "\<BS>"
+				ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"
+				ino <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<BS>"
+			'';
+
+
+			# https://github.com/zbirenbaum/copilot.lua/issues/91#issuecomment-1345190310
 
             extraConfigLua = ''
                     vim.keymap.set("i", '<Tab>', function()
                         if require("copilot.suggestion").is_visible() then
-                        require("copilot.suggestion").accept()
+                        	require("copilot.suggestion").accept()
                         else
-                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+							if vim.fn.pumvisible() == 1 then
+								return '<c-n>'
+							else
+								require("intellitab").indent()
+							end
+							--new_key = vim.fn.pumvisible() == 1 and '<c-n>' or '<tab>'
+                        	--return new_key
+							--vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(new_key, true, false, true), "n", false)
                         end
-                        end, {
+                    end, {
                         silent = true,
                     })
             '';
 
+			extraPlugins = with pkgs.vimPlugins; [
+				# supertab
+			];
 
             globals.mapleader = " ";
 
@@ -60,6 +98,19 @@
 
                     lsp = {
                             enable = true;
+							keymaps = {
+								diagnostic = {
+									"<leader>k" = "goto_prev";
+									"<leader>j" = "goto_next";
+								};
+								lspBuf = {
+									"gd" = "definition";
+									"gD" = "references";
+									"gt" = "type_definition";
+									"gi" = "implementation";
+									"K" = "hover";
+								};
+							};
                             servers = {
                                     nil_ls.enable = true;
                                     rust-analyzer = {
@@ -76,14 +127,18 @@
                             autoStart = true;
                             alwaysComplete = true;
                             installArtifacts = true;
+							recommendedKeymaps = false;
                     };
-                    coq-thirdparty = {
-                        sources = [{
-                            acceptKey = "<TAB>";
-                            short_name = "COP";
-                            src = "copilot";
-                        }];
-                    };
+
+                    # coq-thirdparty = {
+                    #    sources = [{
+                    #        accept_key = "<Tab>";
+                    #        short_name = "COP";
+                    #        src = "copilot";
+                    #    }];
+                    # };
+					intellitab.enable = true;
+					comment-nvim.enable = true;
                     persistence.enable = true;
                     floaterm.enable = true;
                     goyo.enable = true;
@@ -94,6 +149,7 @@
                         enable = true;
                         openOnSetup = true;
                     };
+					treesitter.enable = true;
                     telescope.enable = true;
                     #dashboard.enable = true;
                     #copilot-vim.enable = true;
@@ -102,7 +158,7 @@
                         suggestion = {
                             autoTrigger = true;
                             keymap = {
-                                accept = "<C-i>";
+                                accept = false;
                             };
                         };	
                     };
