3e60850e4887ec9c151282b50bc9b819cdbe50b9
Edward Taylor <dev@edt.nz>
2025-12-18 12:24:00 +1300

    update home and ghostty

--- darwin/home.nix
+++ darwin/home.nix
@@ -11,16 +11,15 @@
 
   home.file.".gitconfig".source = ./.gitconfig;
 
-  home.file.".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/kitty";
+  home.file.".config/kitty".source = ./kitty;
 
   # home.file.".config/karabiner/karabiner.json" = lib.mkIf (karabinerSource != null) {
   #   source = karabinerSource;
   # };
 
-  home.file."Library/Services/Kitty Finder.workflow".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/kitty_finder.workflow";
-  home.file."Library/Services/ghostty_finder.workflow".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/ghostty_finder.workflow";
+  home.file."Library/Services/ghostty_finder.workflow".source = ./ghostty_finder.workflow;
 
   home.file.".config/ghostty/config".source = ./ghostty/config;
 
-  home.file."Library/Preferences/eu.exelban.Stats.plist".source = config.lib.file.mkOutOfStoreSymlink "${configDir}/Stats.plist";
+  # home.file."Library/Preferences/eu.exelban.Stats.plist".source = ./Stats.plist;
 }
