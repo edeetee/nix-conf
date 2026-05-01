679d27e7d13c1d80f9f8067eac64c9452c05f387
Edward Taylor <edeetee@gmail.com>
2026-05-02 09:11:03 +1200

    ndows reboot

--- configuration.nix
+++ configuration.nix
@@ -2,308 +2,323 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running 'nixos-help').
 
-{ config, pkgs, lib, ... }:
 {
-	imports = [
-		./reboot-to-windows.nix
-	];
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  ## DO NOT CHANGE, used for backwards compatibility and upgrade logic
+  system.stateVersion = "23.11"; # Did you read the comment?
 
-## DO NOT CHANGE, used for backwards compatibility and upgrade logic
-	system.stateVersion = "23.11"; # Did you read the comment?
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    systemd-boot.configurationLimit = 5;
+    timeout = 1;
+  };
 
-# Use the systemd-boot EFI boot loader.
-	boot.loader = {
-		systemd-boot.enable = true;
-		efi.canTouchEfiVariables = true;
-		systemd-boot.configurationLimit = 5;
-		timeout = 1;
-	};
+  environment.shellAliases = {
+    nixrs = "sudo nixos-rebuild switch --flake ~/dev/nix-conf/";
+  };
 
-	networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-	networking.nameservers = ["1.1.1.1" "8.8.8.8"];
-	networking.hostName = "homeserver-edt";
-	time.timeZone = "Pacific/Auckland";
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  networking.nameservers = [
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
+  networking.hostName = "homeserver-edt";
+  time.timeZone = "Pacific/Auckland";
 
-#services.devmon.enable = true;
-#services.gvfs.enable = true; 
-	services.udisks2.enable = true;
+  #services.devmon.enable = true;
+  #services.gvfs.enable = true;
+  services.udisks2.enable = true;
 
-	nix.optimise.automatic = true;
-	nix.settings.experimental-features = "nix-command flakes";
+  nix.optimise.automatic = true;
+  nix.settings.experimental-features = "nix-command flakes";
 
-# Enable the X11 windowing system.
-	services.displayManager.gdm = {
-		enable = true;
-		autoSuspend = false;
-	};
+  # Enable the X11 windowing system.
+  services.displayManager.gdm = {
+    enable = true;
+    autoSuspend = false;
+  };
 
-	services.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
 
-	services.zerotierone = {
-		enable = true;
-		joinNetworks = [
-			"56374ac9a48a755c"
-		];
-	};
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = [
+      "56374ac9a48a755c"
+    ];
+  };
 
-	services.avahi = {
-		enable = true;
-		publish.enable = true;
-		publish.userServices = true;
-		publish.addresses = true;
-		publish.domain = true;
-		nssmdns4 = true;
-		publish.workstation = true; # ADDED TO DESKTOP MACHINES
-	};
+  services.avahi = {
+    enable = true;
+    publish.enable = true;
+    publish.userServices = true;
+    publish.addresses = true;
+    publish.domain = true;
+    nssmdns4 = true;
+    publish.workstation = true; # ADDED TO DESKTOP MACHINES
+  };
 
-	security.polkit.extraConfig = ''
-		polkit.addRule(function(action, subject) {
-				if (action.id == "org.freedesktop.login1.suspend" ||
-						action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
-						action.id == "org.freedesktop.login1.hibernate" ||
-						action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
-				{
-				return polkit.Result.NO;
-				}
-				});
-	'';
+  security.polkit.extraConfig = ''
+    		polkit.addRule(function(action, subject) {
+    				if (action.id == "org.freedesktop.login1.suspend" ||
+    						action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+    						action.id == "org.freedesktop.login1.hibernate" ||
+    						action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+    				{
+    				return polkit.Result.NO;
+    				}
+    				});
+    	'';
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
-	users.users.edeetee = {
-		isNormalUser = true;
-		extraGroups = [ "wheel" "video" "networkmanager" "gdm" "render"]; # Enable ‘sudo’ for the user.
-			packages = with pkgs; [
-			];
-	};
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.edeetee = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "video"
+      "networkmanager"
+      "gdm"
+      "render"
+    ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+    ];
+  };
 
-	services.cockpit = {
-		enable = true;
-		port = 9090;
-		settings = {
-			WebService = {
-				AllowUnencrypted = true;
-			};
-		};
-	};
+  services.cockpit = {
+    enable = true;
+    port = 9090;
+    settings = {
+      WebService = {
+        AllowUnencrypted = true;
+      };
+    };
+  };
 
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-	environment.systemPackages = with pkgs; [
-		vim
-			wget
-			git
-			rocmPackages.rocm-smi
-			nvtopPackages.amd
-			blender-hip
-	];
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    rocmPackages.rocm-smi
+    nvtopPackages.amd
+    blender-hip
+  ];
 
+  fonts = {
+    packages = with pkgs; [
+      julia-mono
+    ];
+    fontconfig.defaultFonts = {
+      monospace = [ "Julia Mono" ];
+    };
+  };
 
-	fonts = {
-		packages = with pkgs; [
-			julia-mono
-		];
-		fontconfig.defaultFonts = {
-			monospace = ["Julia Mono"];
-		};
-	};
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
 
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
 
-# Enable the OpenSSH daemon.
-	services.openssh = {
-		enable = true;
-		settings = {
-			PasswordAuthentication = false;
-			KbdInteractiveAuthentication = false;
-		};	
-	};
+  programs.nix-ld.enable = true;
 
+  #  environment.variables = {
+  #      NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+  #        pkgs.stdenv.cc.cc
+  #      ];
+  #      NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+  #  };
 
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-	networking.firewall.enable = false;
+  programs.nixvim.defaultEditor = true;
 
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    plugins = [
+      pkgs.tmuxPlugins.catppuccin
+      pkgs.tmuxPlugins.continuum
+    ];
+  };
 
+  users.defaultUserShell = pkgs.zsh;
 
+  programs.zsh = {
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+  };
 
-	programs.nix-ld.enable = true;
+  boot.supportedFilesystems = [ "ntfs" ];
 
-#  environment.variables = {
-#      NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-#        pkgs.stdenv.cc.cc
-#      ];
-#      NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-#  };
+  #auto delete
 
-	programs.nixvim.defaultEditor = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 
-	programs.tmux = {
-		enable = true;
-		keyMode = "vi";
-		plugins = [pkgs.tmuxPlugins.catppuccin pkgs.tmuxPlugins.continuum];
-	};
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-	users.defaultUserShell = pkgs.zsh;
+  #NETWORK SHARE
 
-	programs.zsh = {
-		autosuggestions.enable = true;
-		syntaxHighlighting.enable = true;
-	};
+  users.users.render = {
+    isNormalUser = true;
+    # isSystemUser = true;
+    group = "render";
+    extraGroups = [
+      "video"
+      "networkmanager"
+      "gdm"
+      "wheel"
+    ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+    ];
+  };
 
+  users.groups.render = {
+  };
 
-	boot.supportedFilesystems = [ "ntfs" ];
+  services.flamenco = {
+    enable = true;
+    role = [
+      "manager"
+      "worker"
+    ];
+    user = "render";
+    listen = {
+      ip = "";
+      port = 8080;
+    };
+    managerConfig = {
+      shared_storage_path = "/mnt/render";
+      variables."blenderArgs".values = [
+        {
+          platform = "all";
+          value = ''-b -y --python-expr "import bpy; c = bpy.context.preferences.addons[\"cycles\"]; cp = c.preferences; cp.compute_device_type = \"HIP\"; print(cp.compute_device_type); cp.get_devices(); [print(x[\"name\"], x[\"use\"]) for x in cp.devices]; print(bpy.data.scenes[0].render.engine); (obj.select_set(True) for obj in bpy.context.scene.objects); bpy.ops.object.simulation_nodes_cache_bake(selected=True)"'';
+        }
+      ];
+    };
+  };
 
-#auto delete
+  # systemd.services.comfyui = {
+  # 	wantedBy = [ "multi-user.target" ];
+  # 	after = [ "network.target" ];
+  # 	serviceConfig = {
+  # 		Type = "simple";
+  # 		ExecStart = "${pkgs.comfyui}/bin/comfyui";
+  # 	};
+  # };
 
-	nix.gc = {
-		automatic = true;
-		dates = "weekly";
-		options = "--delete-older-than 30d";
-	};
+  services.samba-wsdd = {
+    # make shares visible for Windows clients
+    enable = true;
+    # openFirewall = true;
+  };
 
-	boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  # systemd.tmpfiles.rules = [
+  # 	"d /mnt/OPTIPHONIC 1777 edeetee users -"
+  # ];
 
+  # fileSystems."/mnt/OPTIPHONIC" = {
+  # 	fsType = "exfat";
+  # 	device = "/dev/disk/by-uuid/80A1-F7DE";
+  # 	options = [
+  # 		"x-systemd.automount"
+  # 		"nofail"
+  # 		"user"
+  # 		"rw"
+  # 		"umask=1000"
+  # 		"gid=100"
+  # 	];
+  # };
 
-#NETWORK SHARE
+  # system.activationScripts = {
+  # 	optiphonic_mount.text = ''
+  # 		chown -R :users /mnt/OPTIPHONIC
+  # 		# chmod 775 /mnt/OPTIPHONIC
+  # 		'';
+  # };
 
-	users.users.render = {
-		isNormalUser = true;
-# isSystemUser = true;
-		group = "render";
-		extraGroups = [ "video" "networkmanager" "gdm" "wheel"]; # Enable ‘sudo’ for the user.
-			packages = with pkgs; [
-			];
-	};
+  services.samba = {
+    enable = true;
 
-	users.groups.render = {
-	};
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        #use sendfile = yes
+        #max protocol = smb2
+        # note: localhost is the ipv6 localhost ::1
+        #"hosts allow" = "192.168.1. 192.168.0. 127.0.0.1 localhost";
+        #hosts deny = 0.0.0.0/0
+        "guest account" = "render";
+        "map to guest" = "bad user";
 
+        # Performance
+        "socket options" = "TCP_NODELAY SO_SNDBUF=131072 SO_RCVBUF=131072";
+        "use sendfile" = "yes";
+        "min receivefile size" = 16384;
+        "aio read size" = 16384;
+        "aio write size" = 16384;
+      };
 
+      public = {
+        path = "/mnt/render";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0666";
+        "directory mask" = "0777";
+        "force user" = "render";
+        "force group" = "render";
+      };
+      optiphonic = {
+        path = "/mnt/OPTIPHONIC/";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "edeetee";
+        "force group" = "edeetee";
+      };
+      edeetee = {
+        path = "/home/edeetee";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "edeetee";
+        "force group" = "wheel";
+      };
+      windows = {
+        path = "/mnt/windows";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
 
-	services.flamenco = {
-		enable = true;
-		role = ["manager" "worker"];
-		user = "render";
-		listen = {
-			ip = "";
-			port = 8080;
-		};
-		managerConfig = {
-			shared_storage_path = "/mnt/render";
-			variables."blenderArgs".values = [
-			{
-				platform = "all";
-				value = ''-b -y --python-expr "import bpy; c = bpy.context.preferences.addons[\"cycles\"]; cp = c.preferences; cp.compute_device_type = \"HIP\"; print(cp.compute_device_type); cp.get_devices(); [print(x[\"name\"], x[\"use\"]) for x in cp.devices]; print(bpy.data.scenes[0].render.engine); (obj.select_set(True) for obj in bpy.context.scene.objects); bpy.ops.object.simulation_nodes_cache_bake(selected=True)"'';
-			}
-			];
-		};
-	};
-
-# systemd.services.comfyui = {
-# 	wantedBy = [ "multi-user.target" ];
-# 	after = [ "network.target" ];
-# 	serviceConfig = {
-# 		Type = "simple";
-# 		ExecStart = "${pkgs.comfyui}/bin/comfyui";
-# 	};
-# };
-
-	services.samba-wsdd = {
-# make shares visible for Windows clients
-		enable = true;
-# openFirewall = true;
-	};
-
-# systemd.tmpfiles.rules = [
-# 	"d /mnt/OPTIPHONIC 1777 edeetee users -"
-# ];
-
-# fileSystems."/mnt/OPTIPHONIC" = {
-# 	fsType = "exfat";
-# 	device = "/dev/disk/by-uuid/80A1-F7DE";
-# 	options = [
-# 		"x-systemd.automount"
-# 		"nofail"
-# 		"user"
-# 		"rw"
-# 		"umask=1000"
-# 		"gid=100"
-# 	];
-# };
-
-# system.activationScripts = {
-# 	optiphonic_mount.text = ''
-# 		chown -R :users /mnt/OPTIPHONIC
-# 		# chmod 775 /mnt/OPTIPHONIC
-# 		'';
-# };
-
-	services.samba = {
-		enable = true;
-
-		settings = {
-			global = {
-				"workgroup" = "WORKGROUP";
-				"server string" = "smbnix";
-				"netbios name" = "smbnix";
-				"security" = "user"; 
-#use sendfile = yes
-#max protocol = smb2
-# note: localhost is the ipv6 localhost ::1
-#"hosts allow" = "192.168.1. 192.168.0. 127.0.0.1 localhost";
-#hosts deny = 0.0.0.0/0
-				"guest account" = "render";
-				"map to guest" = "bad user";
-
-# Performance
-				"socket options" = "TCP_NODELAY SO_SNDBUF=131072 SO_RCVBUF=131072";
-				"use sendfile" = "yes";
-				"min receivefile size" = 16384;
-				"aio read size" = 16384;
-				"aio write size" = 16384;
-			};
-
-			public = {
-				path = "/mnt/render";
-				browseable = "yes";
-				"read only" = "no";
-				"guest ok" = "yes";
-				"create mask" = "0666";
-				"directory mask" = "0777";
-				"force user" = "render";
-				"force group" = "render";
-			};
-			optiphonic = {
-				path = "/mnt/OPTIPHONIC/";
-				browseable = "yes";
-				"read only" = "no";
-				"guest ok" = "no";
-				"create mask" = "0644";
-				"directory mask" = "0755";
-				"force user" = "edeetee";
-				"force group" = "edeetee";
-			};
-			edeetee = {
-				path = "/home/edeetee";
-				browseable = "yes";
-				"read only" = "no";
-				"guest ok" = "no";
-				"create mask" = "0644";
-				"directory mask" = "0755";
-				"force user" = "edeetee";
-				"force group" = "wheel";
-			};
-			windows = {
-				path = "/mnt/windows";
-				browseable = "yes";
-				"read only" = "no";
-				"guest ok" = "no";
-				"create mask" = "0644";
-				"directory mask" = "0755";
-			};
-
-		};
-	};
+    };
+  };
 
 }
--- reboot-to-windows.nix
+++ reboot-to-windows.nix
@@ -1,25 +1,30 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   reboot-to-windows-script = pkgs.writeShellScriptBin "reboot-to-windows" ''
     #!/usr/bin/env bash
     set -e
-    
+
     # Find Windows Boot Manager entry using efibootmgr
     WINDOWS_ENTRY=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "windows" | head -n 1 | sed 's/Boot\([0-9A-F]*\).*/\1/' || true)
-    
+
     if [ -z "$WINDOWS_ENTRY" ]; then
       echo "Error: Could not find Windows Boot Manager entry"
       echo "Available boot entries:"
       ${pkgs.efibootmgr}/bin/efibootmgr
       exit 1
     fi
-    
+
     echo "Setting next boot to Windows (entry $WINDOWS_ENTRY)"
-    
+
     # Set next boot to Windows using efibootmgr
     ${pkgs.efibootmgr}/bin/efibootmgr --bootnext "$WINDOWS_ENTRY"
-    
+
     # Reboot
     ${pkgs.systemd}/bin/systemctl reboot
   '';
@@ -27,13 +32,13 @@ let
   # Auto-reboot service that checks for display and idle status
   auto-reboot-to-windows-script = pkgs.writeShellScriptBin "auto-reboot-to-windows-check" ''
     #!/usr/bin/env bash
-    
+
     # Check if any display is connected
     if ! ${pkgs.xorg.xrandr}/bin/xrandr | grep -q " connected"; then
       echo "No display connected, skipping auto-reboot"
       exit 0
     fi
-    
+
     # Check if any SSH sessions are active
     if ${pkgs.procps}/bin/pgrep -x sshd > /dev/null; then
       SSH_SESSIONS=$(${pkgs.procps}/bin/pgrep -x sshd | wc -l)
@@ -42,7 +47,7 @@ let
         exit 0
       fi
     fi
-    
+
     # Check if any user is logged in via GDM
     if ${pkgs.systemd}/bin/loginctl list-sessions | grep -q "gdm\|seat"; then
       ACTIVE_SESSIONS=$(${pkgs.systemd}/bin/loginctl list-sessions --no-legend | grep -v "gdm" | wc -l)
@@ -51,15 +56,15 @@ let
         exit 0
       fi
     fi
-    
+
     echo "Display connected, no active sessions. Rebooting to Windows..."
     ${reboot-to-windows-script}/bin/reboot-to-windows
   '';
 in
 {
   # Install the reboot script
-  environment.systemPackages = [ 
-    reboot-to-windows-script 
+  environment.systemPackages = [
+    reboot-to-windows-script
     auto-reboot-to-windows-script
   ];
 
@@ -67,34 +72,82 @@ in
   users.users.windows = {
     isNormalUser = true;
     description = "Reboot to Windows";
-    hashedPassword = ""; # Empty password - passwordless login
+    password = ""; # Empty password - passwordless login
     home = "/var/empty";
     createHome = false;
+    uid = 1001; # Ensure UID is >= 1000 for GDM to show it
+  };
+
+  # Configure AccountsService to show the windows user
+  # environment.etc."gdm/custom.conf".text = ''
+  #   [daemon]
+  #   # Show the windows user in GDM
+  # '';
+
+  security.pam.services.gdm-password.rules.auth.windows = {
+    enable = true;
+    order = config.security.pam.services.gdm-password.rules.auth.unix.order - 50;
+    control = "sufficient";
+    modulePath = "${config.security.pam.package}/lib/security/pam_succeed_if.so";
+    args = [
+      "user"
+      "="
+      "windows"
+    ];
   };
 
   # Configure GDM to allow passwordless login for windows user
-  security.pam.services.gdm-password.text = lib.mkAfter ''
+  # This makes GDM accept empty password for the windows user
+  security.pam.services.gdm-password.text = lib.mkBefore ''
     auth sufficient pam_succeed_if.so user = windows
+    account sufficient pam_succeed_if.so user = windows
+    password sufficient pam_succeed_if.so user = windows
+    session sufficient pam_succeed_if.so user = windows
   '';
 
-  # Create a systemd user service that runs on login for windows user
-  systemd.user.services.reboot-to-windows = {
-    description = "Reboot to Windows on login";
-    wantedBy = [ "default.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.sudo}/bin/sudo ${reboot-to-windows-script}/bin/reboot-to-windows";
-    };
-    # Only run for the windows user
-    unitConfig = {
-      ConditionUser = "windows";
-    };
-  };
+  security.pam.services.gdm-autologin.text = lib.mkBefore ''
+    auth sufficient pam_succeed_if.so user = windows
+    account sufficient pam_succeed_if.so user = windows
+    session sufficient pam_succeed_if.so user = windows
+  '';
+
+  # Create a custom session for the windows user that immediately reboots
+  services.displayManager.sessionPackages = [
+    (pkgs.writeTextFile {
+      name = "reboot-to-windows-session";
+      destination = "/share/wayland-sessions/reboot-to-windows.desktop";
+      text = ''
+        [Desktop Entry]
+        Name=Reboot to Windows
+        Comment=Reboot to Windows
+        Exec=${pkgs.sudo}/bin/sudo ${reboot-to-windows-script}/bin/reboot-to-windows
+        Type=Application
+      '';
+      passthru.providedSessions = [ "reboot-to-windows" ];
+    })
+  ];
+
+  # Make the reboot-to-windows session the default for the windows user
+  # This way when the user is selected, it automatically uses this session
+  systemd.tmpfiles.rules = [
+    "d /var/lib/AccountsService/users 0775 root root -"
+    "f /var/lib/AccountsService/users/windows 0600 root root - -"
+  ];
+
+  # Write AccountsService configuration for the windows user
+  environment.etc."AccountsService/users/windows".text = ''
+    [User]
+    Session=reboot-to-windows
+    SystemAccount=false
+  '';
 
   # Allow the windows user to run reboot commands without password
   security.sudo.extraRules = [
     {
-      users = [ "edeetee" "windows" ];
+      users = [
+        "edeetee"
+        "windows"
+      ];
       commands = [
         {
           command = "${pkgs.efibootmgr}/bin/efibootmgr";
@@ -116,27 +169,27 @@ in
   # Uncomment to enable automatic reboot to Windows when display is connected
   # and there's no user activity for 30 seconds
   /*
-  systemd.services.auto-reboot-to-windows = {
-    description = "Auto-reboot to Windows when display connected and idle";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" "display-manager.service" ];
-    
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${auto-reboot-to-windows-script}/bin/auto-reboot-to-windows-check";
-      User = "root";
-    };
-  };
+    systemd.services.auto-reboot-to-windows = {
+      description = "Auto-reboot to Windows when display connected and idle";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" "display-manager.service" ];
 
-  systemd.timers.auto-reboot-to-windows = {
-    description = "Timer for auto-reboot to Windows check";
-    wantedBy = [ "timers.target" ];
-    
-    timerConfig = {
-      OnBootSec = "30s";
-      OnUnitActiveSec = "30s";
-      Unit = "auto-reboot-to-windows.service";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${auto-reboot-to-windows-script}/bin/auto-reboot-to-windows-check";
+        User = "root";
+      };
     };
-  };
+
+    systemd.timers.auto-reboot-to-windows = {
+      description = "Timer for auto-reboot to Windows check";
+      wantedBy = [ "timers.target" ];
+
+      timerConfig = {
+        OnBootSec = "30s";
+        OnUnitActiveSec = "30s";
+        Unit = "auto-reboot-to-windows.service";
+      };
+    };
   */
 }
