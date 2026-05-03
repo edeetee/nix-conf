2451d721fa6d6cbb1f98378d1f67761021257960
Edward Taylor <edeetee@gmail.com>
2026-05-03 22:40:06 +1200

    simplify amd gpu

--- amd-gpu.nix
+++ amd-gpu.nix
@@ -2,18 +2,13 @@
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
+
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [
-      rocm-opencl-icd
-      rocm-opencl-runtime
-      amdvlk # AMD Vulkan driver
-    ];
-    extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk # 32-bit Vulkan (needed for some games)
-    ];
   };
+
+  nixpkgs.config.rocmSupport = true;
 
   systemd.tmpfiles.rules = [
     "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
