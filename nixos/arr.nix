79c3e52f835bf241b0541874400c1892c99f0e11
Edward Taylor <dev@edt.nz>
2026-05-03 22:43:22 +1200

    fix arr

--- nixos/arr.nix
+++ nixos/arr.nix
@@ -5,20 +5,43 @@
   ...
 }:
 {
-  # services.jellyfin = {
-  #   enable = true;
-  #   openFirewall = true;
-  # };
-  # services.seerr = {
-  #   enable = true;
-  #   openFirewall = true;
-  # };
-  # services.sonarr = {
-  #   enable = true;
-  #   openFirewall = true;
-  # };
-  # services.radarr = {
-  #   enable = true;
-  #   openFirewall = true;
-  # };
+  nixarr = {
+    enable = true;
+    mediaDir = "/mnt/hdd/media";
+    stateDir = "/mnt/hdd/media/.state/nixarr";
+
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    transmission = {
+      enable = true;
+    };
+
+    bazarr = {
+      enable = true;
+      openFirewall = true;
+    };
+    lidarr = {
+      enable = true;
+      openFirewall = true;
+    };
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+    };
+    radarr = {
+      enable = true;
+      openFirewall = true;
+    };
+    sonarr = {
+      enable = true;
+      openFirewall = true;
+    };
+    jellyseerr = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
 }
