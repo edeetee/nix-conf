029f058b0ade64bcf54d806b20ed7c5362340be0
Edward Taylor <dev@edt.nz>
2026-07-24 19:06:51 +1200

    use .local mDNS hostname in homepage links for reliable resolution

--- configuration.nix
+++ configuration.nix
@@ -198,7 +198,7 @@
           {
             "Jellyfin" = {
               icon = "jellyfin.svg";
-              href = "http://${config.networking.hostName}:8096";
+              href = "http://${config.networking.hostName}.local:8096";
               description = "Media server";
               widget = {
                 type = "jellyfin";
@@ -213,14 +213,14 @@
           {
             "Cockpit" = {
               icon = "cockpit.svg";
-              href = "http://${config.networking.hostName}:9090";
+              href = "http://${config.networking.hostName}.local:9090";
               description = "Server management";
             };
           }
           {
             "FileBrowser" = {
               icon = "filebrowser.svg";
-              href = "http://${config.networking.hostName}:8081";
+              href = "http://${config.networking.hostName}.local:8081";
               description = "Web file manager";
             };
           }
@@ -231,7 +231,7 @@
           {
             "Transmission" = {
               icon = "transmission.svg";
-              href = "http://${config.networking.hostName}:9091";
+              href = "http://${config.networking.hostName}.local:9091";
               description = "Torrent client";
               widget = {
                 type = "transmission";
@@ -260,28 +260,28 @@
       {
         "System" = [
           {
-            "Cockpit" = [{abbr = "CP"; href = "http://${config.networking.hostName}:9090";}];
+            "Cockpit" = [{abbr = "CP"; href = "http://${config.networking.hostName}.local:9090";}];
           }
         ];
       }
       {
         "Media" = [
           {
-            "Jellyfin" = [{abbr = "JF"; href = "http://${config.networking.hostName}:8096";}];
+            "Jellyfin" = [{abbr = "JF"; href = "http://${config.networking.hostName}.local:8096";}];
           }
         ];
       }
       {
         "Downloads" = [
           {
-            "Transmission" = [{abbr = "TR"; href = "http://${config.networking.hostName}:9091";}];
+            "Transmission" = [{abbr = "TR"; href = "http://${config.networking.hostName}.local:9091";}];
           }
         ];
       }
       {
         "Files" = [
           {
-            "FileBrowser" = [{abbr = "FB"; href = "http://${config.networking.hostName}:8081";}];
+            "FileBrowser" = [{abbr = "FB"; href = "http://${config.networking.hostName}.local:8081";}];
           }
         ];
       }
